17bedafa0682cc7ac1cdf4450bb4ab3cd6b3cc22
Abhinav Kohar <9128229+abhiKohar@users.noreply.github.com>
2021-01-23 03:17:15 +0000

    Visualizations - 1. target distribution, 2. most frequent words in help messages

--- app/run.py
+++ app/run.py
@@ -1,9 +1,13 @@
 import json
 import plotly
 import pandas as pd
+import re
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords  
 
 import os
 from flask import Flask
@@ -15,6 +19,8 @@ from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
+NUM_WORDS = 30
+
 def tokenize(text):
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
@@ -23,6 +29,11 @@ def tokenize(text):
     for tok in tokens:
         clean_tok = lemmatizer.lemmatize(tok).lower().strip()
         clean_tokens.append(clean_tok)
+    stop_words = set(stopwords.words('english'))  
+  
+      
+
+    clean_tokens = [w for w in clean_tokens if not w in stop_words]  
 
     return clean_tokens
 
@@ -44,6 +55,42 @@ def index():
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
+    df_copy = df.drop(["id","genre","original","message"],axis =1)
+    
+    cols = []
+    x0 = []
+    x1 = []
+    for column in list(df_copy.columns):
+        cols.append(column)
+        x0.append(df[column].value_counts()[0])
+        #print (df[column].value_counts())
+        x1.append(df[column].value_counts()[1])
+        
+    count_words = {}
+    
+    for sent in df["message"]:
+        tokens = tokenize(sent)
+        for w in tokens:
+            if re.findall('[^A-Za-z0-9]',w): # string has special characters ignore it
+                continue;
+            if w in count_words:
+                count_words[w] += 1
+            else:
+                count_words[w] = 1
+    
+    # sort dict by values
+    count_words = sorted(count_words.items(), key=lambda item: item[1])
+#     print (count_words)
+    
+    word_labels = []
+    word_freq = []
+    
+    for i in range (1,NUM_WORDS):
+        word_labels.append(count_words[-i][0])
+    for i in range (1,NUM_WORDS):
+        word_freq.append(count_words[-i][1])
+    
+    
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -64,12 +111,58 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+        {
+            'data': [
+                Bar(
+                    x=cols,
+                    y=x0,
+                    name = "Label 0"
+                    
+                ),
+                Bar(
+                    x=cols,
+                    y=x1,
+                    name = "Label 1"
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Target Labels',
+                'yaxis': {
+                    'title': "Count of Labels"
+                },
+                'xaxis': {
+                    'title': "Category"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=word_labels,
+                    y=word_freq
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Words in Messages',
+                'yaxis': {
+                    'title': "Word Frequency"
+                },
+                'xaxis': {
+                    'title': "Word"
+                }
+            }
         }
     ]
     
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    
+    
     
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
